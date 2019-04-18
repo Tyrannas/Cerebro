cb47a9e7467ae3d00cfb097b91842ac2a539aef1
Alexis Schad <schadoc_alex@hotmail.fr>
2019-04-19 00:43:06 +0200

    config for deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  apps : [{
+    name: 'snake-server',
+    script: 'back/games/snake/server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },{
+    name: 'snake-bots',
+    script: 'clientTemplate/defaultBot.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'aibattle',
+      host : '213.32.64.83',
+      ref  : 'origin/prod',
+      repo : 'git@github.com:Tyrannas/Cerebro.git',
+      path : '/var/www/ai-battle',
+      'post-deploy' : './pm2-build.sh && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
---
+++ pm2-build.sh
@@ -0,0 +1,8 @@
+cd back
+npm i
+cd ..
+
+cd front/snake
+npm i
+npm run build
+cd ..
